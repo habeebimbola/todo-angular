ac5f76f9ab9cf1c3ff3e0bb7ef4bf99132170eea
ANIMASHAUN HABEEB <animashaunhabeeb@ANIMASHAUNs-MBP.Home>
2024-03-17 18:37:40 +0000

    Implementing Angular JavaScript Framework

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'todo-app-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-app-angular');
+    expect(app.title).toEqual('You are welcome to the world of Angular Progamming!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todo-app-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('You are welcome to the world of Angular Progamming!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-app-angular';
+  title = 'You are welcome to the world of Angular Progamming!';
+
+  filter : "all" | "actuve" | "done" = "all";
+
+  allItems = [{ description: "Sleep", done: true},
+              {description: "Eat", done: false},
+              {description: "Drink", done: false},
+              {description: "Laugh", done: false}];
+
+  get items()
+  {
+      if(this.filter === "all"){
+        return this.allItems;
+      }
+      return this.allItems.filter((item) =>{ return this.filter === "done"?  item.done: !item.done });
+  }
+
+  addItem(description:string){
+     this.allItems.unshift({description, done:false});
+  }
+ 
 }
